1f123b6e5dd0e0a2264cccb4f79d5d126b98d3b2
Code-aadi <kouravaditya7@gmail.com>
2026-06-18 12:17:00 +0530

    port

--- Backend/transport.js
+++ Backend/transport.js
@@ -1,9 +1,11 @@
 import nodemailer from "nodemailer"
 const transport = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.gmail,
-        pass:process.env.email_app_pass,
-    }
-})
+  service: "gmail",
+  auth: {
+    user: process.env.gmail,
+    pass: process.env.email_app_pass,
+  },
+  secure: false,
+  port: 587
+});
 export default transport
